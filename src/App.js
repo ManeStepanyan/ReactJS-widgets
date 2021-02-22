03c5c54e93d46dbd37c77b1099daea47585658ac
manestep <manestep@gmail.com>
2021-02-22 22:57:14 +0400

    add component rendering

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Accordian from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
     {
@@ -31,14 +33,27 @@ const options = [{
     value: 'blue'
 }];
 
+
 export default () => {
     const [selected, setSelected] = useState(options[0]);
-    const [showDropdown, setShowDropdown] = useState(true);
+    //const [showDropdown, setShowDropdown] = useState(true);
     return (
-    <div>
-    
-        {/*<Search /> */
-        /*<Accordian items={items}/>*/}
-        <Translate />
+    <div>     
+        <Header />   
+        <Route path="/">
+            <Accordian items={items} />
+        </Route>
+        <Route path="/list">
+            <Search />
+        </Route>
+        <Route path="/dropdown">
+            <Dropdown label="Select a color"
+            options={options}
+            selected={selected}
+            onSelectedChange={setSelected}  />
+        </Route>
+        <Route path="/translate">
+            <Translate />
+        </Route>
     </div>);
 };
